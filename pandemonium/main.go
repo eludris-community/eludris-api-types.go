5309998b031332fadd0fc5a774fb2379b718fe66
Sham <thesham@protonmail.com>
2023-02-28 21:11:30 +0200

    feat: implement pandemonium types

---
+++ pandemonium/main.go
@@ -0,0 +1,23 @@
+package pandemonium
+
+import (
+	"github.com/toolifelesstocode/eludris-api-types/oprish"
+)
+
+
+type Ping struct {
+	// Always `"PING"`
+	Op string `json:"op"`
+}
+
+type Pong struct {
+	// Always `"PONG"`
+	Op string `json:"op"`
+}
+
+type MessageCreate struct {
+	// Always `"MESSAGE_CREATE"`
+	Op string `json:"op"`
+	// The message object that triggered the event.
+	D oprish.Message `json:"d"`
+}
